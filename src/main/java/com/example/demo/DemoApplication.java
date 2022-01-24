bfaaa38e891970784e45b470f094a72e6e6e1a0f
Gilberto Pucciarelli <gilberto.pucciarelli@gft.com>
2022-01-24 15:40:13 +0100

    Added method root() mapped to / path

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -17,4 +17,9 @@ public class DemoApplication {
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 	}
+
+	@GetMapping("/")
+	public String root() {
+		return String.format("This is the root path!");
+	}
 }
