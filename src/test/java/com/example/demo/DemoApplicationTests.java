5b7d5af6479c6a90d3935bde578b058d4b92b925
Gilberto Pucciarelli <gilberto.pucciarelli@gft.com>
2022-01-27 12:47:23 +0100

    Added rootTest

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
+	
+	@Test
+	void rootTest(@Autowired TestRestTemplate restTemplate) {
+		assertThat(restTemplate.getForObject("/", String.class))
+				.isEqualTo("This is the root path!");
+	}
 }
